d95ba6d8e43b0167480f236141d7fe300d051204
wesleysnipes13 <135257804+wesleysnipes13@users.noreply.github.com>
2025-12-18 22:11:42 +0000

    prepare for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 'base: "./"' ensures assets are loaded correctly regardless of the repository name
-  base: './',
+  // Set base to the GitHub Pages repo path so assets load at /OWNER/REPO/
+  base: '/boland-index/',
 });
